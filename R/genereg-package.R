4284895981a62973dcba6744fdbffe9e3d4fcb1d
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-02-08 22:10:55 +0000

    Quiet concerns of R CMD check regarding "." in pipelines

---
+++ R/genereg-package.R
@@ -0,0 +1,11 @@
+#' \code{genereg} package
+#'
+#' See the README on
+#' \href{https://github.com/ruthkr/genereg/}{GitHub}
+#'
+#' @docType package
+#' @name genereg
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))

89311613eecf4648239d235ce7ef4d800feaf974
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-06-21 11:35:07 +0100

    changed *.mean and *.sd to data_align and data_target *_mean and *_sd

--- R/calculate_best_shifts_stretches.R
+++ R/calculate_best_shifts_stretches.R
@@ -99,12 +99,12 @@ get_best_shift <- function(curr_sym,
   num_shifts <- 25 # the number of different shifts to be considered.
 
 
-  test <- test[test$locus_name==curr_sym, ]
+  test <- test[test$locus_name == curr_sym, ]
 
   # transform timepoint to be time from first timepoint
-  test[, delta_time:=timepoint - min(timepoint), by=.(accession)]
+  test[, delta_time := timepoint - min(timepoint), by = .(accession)]
   # apply stretch_factor to the arabidopsis, leave the rapa as is
-  test$delta_time[test$accession == 'Col0'] <- test$delta_time[test$accession=='Col0'] * stretch_factor
+  test$delta_time[test$accession == accession_data_to_align] <- test$delta_time[test$accession == accession_data_to_align] * stretch_factor
 
 
   # # Try to make it consistent as in apply stretch
@@ -112,23 +112,22 @@ get_best_shift <- function(curr_sym,
   # test$delta_time[test$accession=='Ro18'] <- test$delta_time[test$accession=='Ro18'] + 14
 
   all_scores <- rep(0, num_shifts)
-  all.ara.mean <- rep(0, num_shifts)
-  all.bra.mean <- rep(0, num_shifts)
-  all.ara.sd <- rep(0, num_shifts)
-  all.bra.sd <- rep(0, num_shifts)
+  all_data_align_mean <- rep(0, num_shifts)
+  all_data_target_mean <- rep(0, num_shifts)
+  all_data_align_sd <- rep(0, num_shifts)
+  all_data_target_sd <- rep(0, num_shifts)
 
-  all_shifts <- seq(min_shift, max_shift, length_out=num_shifts)
+  all_shifts <- seq(min_shift, max_shift, length_out = num_shifts)
   if (!(0 %in% all_shifts)) {
     all_shifts <- c(all_shifts, 0) # include 0 shift in candidates.
   }
-  i=1
+  i <- 1
   for (i in 1:length(all_shifts)) {
-
     curr_shift <- all_shifts[i]
 
     # shift the arabidopsis expression timings
     test$shifted_time <- test$delta_time
-    test$shifted_time[test$accession == 'Col0'] <- test$delta_time[test$accession == 'Col0'] + curr_shift
+    test$shifted_time[test$accession == accession_data_to_align] <- test$delta_time[test$accession == accession_data_to_align] + curr_shift
 
     #### test plot - of shifted, UNNORMALISED gene expression
     # if (testing==TRUE) {
@@ -143,78 +142,79 @@ get_best_shift <- function(curr_sym,
 
     # Cut down to just the arabidopsis and brassica timepoints which compared
     test <- get_compared_timepoints(test)
-    compared <- test[test$is.compared==TRUE, ]
+    compared <- test[test$is.compared == TRUE, ]
 
     # Renormalise expression using just these timepoints?
     if (do_rescale == TRUE) {
       # record the mean and sd of the compared points, used for rescaling
       # in "apply shift" function
-      ara.mean <- mean(compared$mean_cpm[compared$accession=='Col0'])
-      bra.mean <- mean(compared$mean_cpm[compared$accession=='Ro18'])
-      ara.sd <- stats::sd(compared$mean_cpm[compared$accession=='Col0'])
-      bra.sd<- stats::sd(compared$mean_cpm[compared$accession=='Ro18'])
+      data_align_mean <- mean(compared$mean_cpm[compared$accession == accession_data_to_align])
+      data_target_mean <- mean(compared$mean_cpm[compared$accession == accession_data_target])
+      data_align_sd <- stats::sd(compared$mean_cpm[compared$accession == accession_data_to_align])
+      data_target_sd <- stats::sd(compared$mean_cpm[compared$accession == accession_data_target])
 
       # do the transformation for here
-      if ((ara.sd != 0 | !is.nan(ara.sd)) & (bra.sd != 0 | !is.nan(bra.sd))) { # if neither are 0, so won't be dividing by 0 (which gives NaNs)
-        compared[, mean_cpm:=scale(mean_cpm, scale=TRUE, center=TRUE), by=.(accession)]
+      if ((data_align_sd != 0 | !is.nan(data_align_sd)) & (data_target_sd != 0 | !is.nan(data_target_sd))) { # if neither are 0, so won't be dividing by 0 (which gives NaNs)
+        compared[, mean_cpm := scale(mean_cpm, scale = TRUE, center = TRUE), by = .(accession)]
       } else { # if at least one of them is all 0
-        ara.compared <- compared[compared$accession=='Col0',]
-        bra.compared <- compared[compared$accession=='Ro18',]
-        if((ara.sd == 0) & (bra.sd != 0 | !is.nan(bra.sd))) { # if only ara.sd==0
-          bra.compared[, mean_cpm:=scale(mean_cpm, scale=TRUE, center=TRUE), by=.(accession)]
+        ara.compared <- compared[compared$accession == accession_data_to_align, ]
+        bra.compared <- compared[compared$accession == accession_data_target, ]
+        if ((data_align_sd == 0) & (data_target_sd != 0 | !is.nan(data_target_sd))) { # if only data_align_sd==0
+          bra.compared[, mean_cpm := scale(mean_cpm, scale = TRUE, center = TRUE), by = .(accession)]
         }
-        if ((ara.sd != 0 | !is.nan(ara.sd))  & (bra.sd == 0)) { # if only bra.sd == 0
-          ara.compared[, mean_cpm:=scale(mean_cpm, scale=TRUE, center=TRUE), by=.(accession)]
+        if ((data_align_sd != 0 | !is.nan(data_align_sd)) & (data_target_sd == 0)) { # if only data_target_sd == 0
+          ara.compared[, mean_cpm := scale(mean_cpm, scale = TRUE, center = TRUE), by = .(accession)]
         }
         # if both are all 0, then do nothing.
         compared <- rbind(ara.compared, bra.compared)
       }
-
     } else {
       # if didn't rescale expression for comparison, record values s.t. (x - xmean) / x_sd = x
-      ara.mean <- 0
-      bra.mean <- 0
-      ara.sd <- 1
-      bra.sd<- 1
+      data_align_mean <- 0
+      data_target_mean <- 0
+      data_align_sd <- 1
+      data_target_sd <- 1
     }
 
     ### test plot of shifted, and normalised gene expression
-    if (testing==TRUE) {
-      p <- ggplot2::ggplot(compared)+
-        ggplot2::aes(x=shifted_time, y=mean_cpm, color=accession)+
-        ggplot2::geom_point()+
-        ggplot2::ggtitle(paste0('shift : ', curr_shift))
-      ggplot2::ggsave(paste0('./testing/',stretch_factor, '-', curr_shift, '.pdf'))
+    if (testing == TRUE) {
+      p <- ggplot2::ggplot(compared) +
+        ggplot2::aes(x = shifted_time, y = mean_cpm, color = accession) +
+        ggplot2::geom_point() +
+        ggplot2::ggtitle(paste0("shift : ", curr_shift))
+      ggplot2::ggsave(paste0("./testing/", stretch_factor, "-", curr_shift, ".pdf"))
     }
 
     # for each arabidopsis timepoint, linear interpolate between the two nearest brassica timepoints
-    ara.compared <- compared[compared$accession=='Col0']
-    bra.compared <- compared[compared$accession=='Ro18']
+    ara.compared <- compared[compared$accession == accession_data_to_align]
+    bra.compared <- compared[compared$accession == accession_data_target]
 
-    ara.compared$pred.bra.expression <- sapply(ara.compared$shifted_time, interpolate_brassica_comparison_expression, bra.dt=bra.compared)
+    ara.compared$pred.bra.expression <- sapply(ara.compared$shifted_time, interpolate_brassica_comparison_expression, bra.dt = bra.compared)
 
     # calculate the score, using the (interpolated) predicted.bra.expression, and the observed arabidopsis expression
     # score = mean ((observed - expected)**2 )
     score <- calc_score(ara.compared$mean_cpm, ara.compared$pred.bra.expression)
 
     if (is.na(score)) {
-      print('error in get_best_shift(): got a score of NA for gene:')
+      print("error in get_best_shift(): got a score of NA for gene:")
       print(paste("ara.compared$mean_cpm:", ara.compared$mean_cpm))
       print(ara.compared$pred.bra.expression)
       print(curr_sym)
-      print(paste0('with curr_shift=', curr_shift))
+      print(paste0("with curr_shift=", curr_shift))
       stop()
     }
 
     all_scores[i] <- score
-    all.ara.mean[i] <- ara.mean
-    all.bra.mean[i] <- bra.mean
-    all.ara.sd[i] <- ara.sd
-    all.bra.sd[i] <- bra.sd
+    all_data_align_mean[i] <- data_align_mean
+    all_data_target_mean[i] <- data_target_mean
+    all_data_align_sd[i] <- data_align_sd
+    all_data_target_sd[i] <- data_target_sd
   }
 
-  out <- data.table::data.table(data.frame('gene'=curr_sym, 'stretch'=stretch_factor, 'shift'=all_shifts, 'score'=all_scores,
-                                           'ara.compared.mean'=all.ara.mean, 'bra.compared.mean'=all.bra.mean,
-                                           'ara.compared.sd'=all.ara.sd, 'bra.compared.sd'=all.bra.sd))
+  out <- data.table::data.table(data.frame(
+    "gene" = curr_sym, "stretch" = stretch_factor, "shift" = all_shifts, "score" = all_scores,
+    "ara.compared.mean" = all_data_align_mean, "bra.compared.mean" = all_data_target_mean,
+    "ara.compared.sd" = all_data_align_sd, "bra.compared.sd" = all_data_target_sd
+  ))
   return(out)
 }

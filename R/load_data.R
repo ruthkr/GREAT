1692b086b96ea0e31534a01319bbe1290af7ff55
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-09-28 23:55:34 +0100

    Minor style change in load_data.R functions

--- R/load_data.R
+++ R/load_data.R
@@ -100,11 +100,15 @@ get_expression_of_interest <- function(filepath_data_fix,
   # Reformat depending on how want to compare candidate transformed data to fix data, using individual fix data, or summed fix data genes
   # Example: if want to used summed brassica data to compare to the brassica: get symbol level expression total. Locus_name identity is ATG id
   # Otherwise duplicate each arabidopsis, so have an arabidopis copy for each brassica CDS gene. Now locus_name identity is CDS.model
-  if (sum_exp_data_fix == T) {
-    exp <- stats::aggregate(norm.cpm~sample_id+accession+tissue+timepoint+dataset+group+locus_name, data=exp, sum)
-  } else if (sum_exp_data_fix == F) {
-    exp_data_to_transform <- exp[exp$accession == accession_data_to_transform,]
-    exp_data_fix <- exp[exp$accession != accession_data_to_transform,]
+  if (sum_exp_data_fix) {
+    exp <- stats::aggregate(
+      formula = norm.cpm ~ sample_id + accession + tissue + timepoint + dataset + group + locus_name,
+      data = exp,
+      FUN = sum
+    )
+  } else {
+    exp_data_to_transform <- exp[exp$accession == accession_data_to_transform, ]
+    exp_data_fix <- exp[exp$accession != accession_data_to_transform, ]
 
     ids_data_fix <- unique(exp_data_fix[, ..ids_data_fix_colnames])
     # exp_data_to_transform$CDS.model <- NULL
@@ -112,7 +116,12 @@ get_expression_of_interest <- function(filepath_data_fix,
     exp_data_to_transform <- exp_data_to_transform %>%
       dplyr::select(-c(fix_id_table_shared_colname))
 
-    exp_data_to_transform <- merge(ids_data_fix, exp_data_to_transform, by = fix_and_to_transform_data_shared_colname, allow.cartesian=T)
+    exp_data_to_transform <- merge(
+      ids_data_fix,
+      exp_data_to_transform,
+      by = fix_and_to_transform_data_shared_colname,
+      allow.cartesian = TRUE
+    )
 
     # Define the same ID and locus name for each data
     exp_data_to_transform <- exp_data_to_transform %>%
@@ -127,12 +136,10 @@ get_expression_of_interest <- function(filepath_data_fix,
         locus_name = get(fix_id_table_shared_colname)
       )
 
-
     # exp_data_to_transform$ara.id <- exp_data_to_transform$locus_name
     # exp_data_to_transform$locus_name <- exp_data_to_transform$CDS.model
     # exp_data_fix$ara.id <- exp_data_fix$locus_name
     # exp_data_fix$locus_name <- exp_data_fix$CDS.model
-
 
     exp <- rbind(exp_data_to_transform, exp_data_fix)
   }
@@ -156,7 +163,7 @@ get_all_data <- function(filepath_data_fix,
   data_fix <- readRDS(filepath_data_fix)
   data_to_transform <- readRDS(filepath_data_to_transform)
 
-  if (tools::file_ext(filepath_id_table) == "csv"){
+  if (tools::file_ext(filepath_id_table) == "csv") {
     id_table <- data.table::fread(filepath_id_table)
   } else {
     id_table <- readRDS(filepath_id_table)
@@ -164,7 +171,7 @@ get_all_data <- function(filepath_data_fix,
 
   # Take unique id_table
   id_table_unique <- unique(id_table[, ..colnames_id_table]) %>%
-    dplyr:: mutate_all(.funs = toupper)
+    dplyr::mutate_all(.funs = toupper)
 
   # Filtering NA values in dataframe of table id
   # dplyr::filter(!is.na(locus_name), !locus_name %in% c("", "-"))
@@ -177,13 +184,20 @@ get_all_data <- function(filepath_data_fix,
   data_to_transform[, (fix_and_to_transform_data_shared_colname) := data_to_transform[[fix_id_table_shared_colname]]]
 
   # Cut down to only have genes with ATG locus present in both datasets
-  common_symbols <- intersect(data_to_transform[[fix_and_to_transform_data_shared_colname]], data_fix[[fix_and_to_transform_data_shared_colname]])
+  common_symbols <- intersect(
+    data_to_transform[[fix_and_to_transform_data_shared_colname]],
+    data_fix[[fix_and_to_transform_data_shared_colname]]
+  )
+
   data_to_transform <- data_to_transform[data_to_transform[[fix_and_to_transform_data_shared_colname]] %in% common_symbols, ]
   data_fix <- data_fix[data_fix[[fix_and_to_transform_data_shared_colname]] %in% common_symbols, ]
 
   # Take a common columns
   if (is.null(colnames_wanted)) {
-    colnames_wanted <- intersect(colnames(data_to_transform), colnames(data_fix))
+    colnames_wanted <- intersect(
+      colnames(data_to_transform),
+      colnames(data_fix)
+    )
   } else {
     colnames_wanted <- colnames_wanted
   }
@@ -199,27 +213,32 @@ get_all_data <- function(filepath_data_fix,
 
 #' @export
 shorten_groups <- function(exp) {
-
   # Get reps for klepikova and for brassica data, make sure it is a data.table
   exp <- data.table::data.table(exp)
 
   # Get the last element of "sample_id" of Brassica data
-  B <- exp[exp$accession != 'Col0']
-  B[, c('j1', 'j2', 'j3', 'j4', 'j5', 'j6', 'rep'):=data.table::tstrsplit(sample_id, split='_')]
+  B <- exp[exp$accession != "Col0"]
+  B[, c("j1", "j2", "j3", "j4", "j5", "j6", "rep") := data.table::tstrsplit(sample_id, split = "_")]
   B$rep[is.na(B$rep)] <- 1
-  B[, c('j1', 'j2', 'j3', 'j4', 'j5', 'j6')] <- NULL
+  B[, c("j1", "j2", "j3", "j4", "j5", "j6")] <- NULL
 
   # Get the last element of "dataset" of Arabidopsis data
-  A <- exp[exp$accession == 'Col0']
-  A[, c('j1', 'j2','rep'):=data.table::tstrsplit(dataset, split='_')]
-  A[, c('j1', 'j2')] <- NULL
+  A <- exp[exp$accession == "Col0"]
+  A[, c("j1", "j2", "rep") := data.table::tstrsplit(dataset, split = "_")]
+  A[, c("j1", "j2")] <- NULL
 
   # Combine both data
-  exp <- rbind(B,A)
+  exp <- rbind(B, A)
 
-  exp$ds <- as.character(factor(exp$rep, levels=c('1', '2', '3', '4'), labels=c('a', 'b', 'c', 'd')))
+  exp$ds <- factor(
+    exp$rep,
+    levels = c("1", "2", "3", "4"),
+    labels = c("a", "b", "c", "d")
+  ) %>%
+    as.character()
 
   exp <- data.table::data.table(exp)
-  exp[, group:=paste(accession, sprintf('%02d', timepoint), ds, sep='-')]
+  exp[, group := paste(accession, sprintf("%02d", timepoint), ds, sep = "-")]
+
   return(exp)
 }

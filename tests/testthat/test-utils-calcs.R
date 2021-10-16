3090dd0c9518fbd13f5c1a0c83ab4412fd081e75
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-10-16 18:59:06 +0100

    Added unit tests for utils-calcs.R

---
+++ tests/testthat/test-utils-calcs.R
@@ -0,0 +1,16 @@
+test_that("calc_AIC works", {
+  expect_equal(class(calc_AIC(-3, 4)), "numeric")
+})
+
+test_that("calc_BIC works", {
+  expect_equal(class(calc_BIC(-3, 4, 1)), "numeric")
+})
+
+test_that("calc_score works", {
+  set.seed(1)
+  data_to_transform_expression <- rnorm(5)
+  data_ref_expression <- rnorm(5)
+  score <- calc_score(data_to_transform_expression, data_ref_expression)
+  expect_equal(class(score), "numeric")
+  expect_gte(score, 0)
+})

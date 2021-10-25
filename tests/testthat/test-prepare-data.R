e88e1b2ffb00e1938ef76ad796c25d9283dfa2ac
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-10-25 09:43:42 +0100

    added readRDS when reading data

--- tests/testthat/test-prepare-data.R
+++ tests/testthat/test-prepare-data.R
@@ -1,12 +1,12 @@
-id_table_path <- system.file("extdata/sample_data/id_table.RDS", package = "GREAT")
-arabidopsis_expression_path <- system.file("extdata/sample_data/arabidopsis_expression.RDS", package = "GREAT")
-brassica_rapa_expression_path <- system.file("extdata/sample_data/brassica_rapa_expression.RDS", package = "GREAT")
+id_table <- system.file("extdata/sample_data/id_table.RDS", package = "GREAT") %>% readRDS()
+arabidopsis_expression <- system.file("extdata/sample_data/arabidopsis_expression.RDS", package = "GREAT") %>% readRDS()
+brassica_rapa_expression <- system.file("extdata/sample_data/brassica_rapa_expression.RDS", package = "GREAT") %>% readRDS()
 
 test_that("get_mean_and_all_exp_data works", {
   test_load_all_and_mean_df <- get_mean_and_all_exp_data(
-    filepath_data_ref = brassica_rapa_expression_path,
-    filepath_data_to_transform = arabidopsis_expression_path,
-    filepath_id_table = id_table_path,
+    data_ref = brassica_rapa_expression,
+    data_to_transform = arabidopsis_expression,
+    id_table = id_table,
     fix_id_table_shared_colname = "CDS.model",
     fix_and_to_transform_data_shared_colname = "locus_name",
     colnames_id_table = c("CDS.model", "symbol", "locus_name"),

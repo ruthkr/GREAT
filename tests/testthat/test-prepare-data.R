734d83d5594757f9f42b62b0e40b5ea5642d3219
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-10-26 15:29:29 +0100

    Minor style fix in prepare-data unit tests

--- tests/testthat/test-prepare-data.R
+++ tests/testthat/test-prepare-data.R
@@ -5,34 +5,36 @@ brassica_rapa_expression <- system.file("extdata/sample_data/brassica_rapa_expre
 all_data_df <- system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "GREAT") %>%
   utils::read.csv() %>%
   as.data.table()
+
 mean_df <- system.file("extdata/brapa_arabidopsis_mean.csv", package = "GREAT") %>%
   utils::read.csv() %>%
   as.data.table()
 
-
 test_that("get_expression_of_interest works", {
-exp <- get_expression_of_interest(
-      data_ref = brassica_rapa_expression,
-      data_to_transform = arabidopsis_expression,
-      id_table = id_table,
-      fix_id_table_shared_colname = "CDS.model",
-      fix_and_to_transform_data_shared_colname = "locus_name",
-      colnames_id_table = c("CDS.model", "symbol", "locus_name"),
-      colnames_wanted = NULL,
-      tissue_wanted = "apex",
-      curr_GoIs = c("AT1G69120", "AT5G618"),
-      sum_exp_data_ref = FALSE,
-      accession_data_to_transform = "Col0",
-      ids_data_ref_colnames = c("CDS.model", "locus_name")
-      )
+  exp <- get_expression_of_interest(
+    data_ref = brassica_rapa_expression,
+    data_to_transform = arabidopsis_expression,
+    id_table = id_table,
+    fix_id_table_shared_colname = "CDS.model",
+    fix_and_to_transform_data_shared_colname = "locus_name",
+    colnames_id_table = c("CDS.model", "symbol", "locus_name"),
+    colnames_wanted = NULL,
+    tissue_wanted = "apex",
+    curr_GoIs = c("AT1G69120", "AT5G618"),
+    sum_exp_data_ref = FALSE,
+    accession_data_to_transform = "Col0",
+    ids_data_ref_colnames = c("CDS.model", "locus_name")
+  )
 
   expect_equal(class(exp)[[1]], "data.table")
 })
 
 test_that("get_mean_data works", {
-  mean_data <- get_mean_data(all_data_df,
-              max_expression_value_wanted = 5,
-              accession_data_to_transform = "Col0")
+  mean_data <- get_mean_data(
+    all_data_df,
+    max_expression_value_wanted = 5,
+    accession_data_to_transform = "Col0"
+  )
 
   expect_equal(
     round(mean_data$expression_value, 5),

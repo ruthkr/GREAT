35ca8d4990b743f714429f97ed3a380bb0ea435a
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2022-01-06 11:28:27 +0000

    Added "..variable_name" {data.table} constructs into globalVariables() to remove "no visible binding for global variable" errors

--- R/GREAT-package.R
+++ R/GREAT-package.R
@@ -9,4 +9,6 @@
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
+if (getRversion() >= "2.15.1") utils::globalVariables(
+  c(".", "..colnames_id_table", "..ids_data_ref_colnames", "..colnames_wanted")
+)

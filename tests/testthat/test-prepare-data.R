29264208876789847a04582a4542ef25b84c1124
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2022-01-11 12:23:34 +0000

    Renamed .RDS sample data files to .rds

--- tests/testthat/test-prepare-data.R
+++ tests/testthat/test-prepare-data.R
@@ -1,6 +1,6 @@
-id_table <- system.file("extdata/sample_data/id_table.RDS", package = "greatR") %>% readRDS()
-arabidopsis_expression <- system.file("extdata/sample_data/arabidopsis_expression.RDS", package = "greatR") %>% readRDS()
-brassica_rapa_expression <- system.file("extdata/sample_data/brassica_rapa_expression.RDS", package = "greatR") %>% readRDS()
+id_table <- system.file("extdata/sample_data/id_table.rds", package = "greatR") %>% readRDS()
+arabidopsis_expression <- system.file("extdata/sample_data/arabidopsis_expression.rds", package = "greatR") %>% readRDS()
+brassica_rapa_expression <- system.file("extdata/sample_data/brassica_rapa_expression.rds", package = "greatR") %>% readRDS()
 
 all_data_df <- system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR") %>%
   utils::read.csv() %>%

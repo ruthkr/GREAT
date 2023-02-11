eca55664ad0d79ca6bb38ff84426154beffb7189
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-11 23:45:06 +0000

    Added compare_dynamics_H2() and compare_dynamics_H1() functions to calculate BIC for hypotheses H1 and H2

---
+++ R/compare_dynamics.R
@@ -0,0 +1,54 @@
+compare_dynamics_H2 <- function(data) {
+  # Cut down to the data for each model
+  data_query <- data[data$accession == "query"]
+  data_ref <- data[data$accession == "ref"]
+
+  # Fit using cubic spline with K+3 params where K=num.knots as can omit constant term
+  num_spline_params <- 6 # number of parameters for each spline fitting (degree and this used to calculate num knots)
+  num_obs <- nrow(data)
+
+  # Fit models for reference and query
+  fit_ref <- stats::lm(
+    expression_value ~ splines::bs(timepoint, df = num_spline_params, degree = 3),
+    data = data_ref
+  )
+  fit_query <- stats::lm(
+    expression_value ~ splines::bs(timepoint, df = num_spline_params, degree = 3),
+    data = data_query
+  )
+
+  # Calculate the log likelihoods and BIC
+  loglik_query <- calc_loglik(fit_query, data_query)
+  loglik_ref <- calc_loglik(fit_ref, data_ref)
+  loglik_separate <- loglik_query + loglik_ref
+
+  BIC <- calc_BIC(loglik_separate, 2 * num_spline_params, num_obs)
+
+  return(BIC)
+}
+
+compare_dynamics_H1 <- function(data) {
+  # Cut down to the data for each model
+  data_query <- data[data$accession == "query"]
+  data_ref <- data[data$accession == "ref"]
+
+  # Fit using cubic spline with K+3 params where K=num.knots as can omit constant term
+  num_spline_params <- 6 # number of parameters for each spline fitting (degree and this used to calculate num knots)
+  num_registration_params <- 2 # stretch, shift
+  num_obs <- nrow(data)
+
+  # Fit model for combined data together
+  fit_all <- stats::lm(
+    expression_value ~ splines::bs(timepoint, df = num_spline_params, degree = 3),
+    data = data
+  )
+
+  # Calculate the log likelihoods and BIC
+  loglik_ref <- calc_loglik(fit_all, data_ref)
+  loglik_query <- calc_loglik(fit_all, data_query)
+  loglik_combined <- loglik_query + loglik_ref
+
+  BIC <- calc_BIC(loglik_combined, num_spline_params + num_registration_params, num_obs)
+
+  return(BIC)
+}

00c9b48742823f6c457caa51d06d8fa4ebcabaf9
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-12 07:59:24 +0000

    Removed old unit tests and updated Suggests to testthat (>= 3.0.0) in DESCRIPTION file

--- tests/testthat/test-utils-calcs.R
+++ tests/testthat/test-utils-calcs.R
@@ -1,12 +1,3 @@
 test_that("calc_BIC works", {
   expect_equal(class(calc_BIC(-3, 4, 1)), "numeric")
 })
-
-test_that("calc_score works", {
-  set.seed(1)
-  data_to_transform_expression <- rnorm(5)
-  data_ref_expression <- rnorm(5)
-  score <- calc_score(data_to_transform_expression, data_ref_expression)
-  expect_equal(class(score), "numeric")
-  expect_gte(score, 0)
-})

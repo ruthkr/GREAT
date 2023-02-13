635c464b0f4770e96addb5b021fa01085a80bbdb
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-13 21:23:37 +0000

    Rewrote test-utils-calcs.R using {data.table}, and added additional checks for calc_loglik() and calc_BIC()

--- tests/testthat/test-utils-calcs.R
+++ tests/testthat/test-utils-calcs.R
@@ -1,22 +1,26 @@
-# # Get data where have expression data
-# # and fit with a model
-# sample_data <- system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR") |>
-#   utils::read.csv() |>
-#   dplyr::select(
-#     timepoint,
-#     expression_value
-#   ) |>
-#   head(10)
-#
-# fit_model <- lm(timepoint ~ expression_value, data = sample_data)
-#
-# test_that("calc_loglik works", {
-#   expect_equal(class(calc_loglik(fit_model, sample_data)), "numeric")
-# })
-#
-#
-# test_that("calc_BIC works", {
-#   expect_equal(class(calc_BIC(calc_loglik(fit_model, sample_data), 2, 10)), "numeric")
-# })
-#
-#
+# Get expression data and fit with a model
+sample_data <- data.table::fread(system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR"))
+sample_data <- sample_data[1:10, .(timepoint, expression_value)]
+
+fit_model <- stats::lm(timepoint ~ expression_value, data = sample_data)
+
+test_that("calc_loglik works", {
+  loglik <- calc_loglik(fit_model, sample_data)
+
+  # Expected outputs
+  expect_equal(class(loglik), "numeric")
+  expect_lte(loglik, 0)
+  expect_equal(loglik, -3.777704, tolerance = 1e-6)
+})
+
+test_that("calc_BIC works", {
+  loglik <- calc_loglik(fit_model, sample_data)
+  bic <- calc_BIC(loglik, 2, 10)
+
+  # Expected outputs
+  expect_equal(class(bic), "numeric")
+  expect_gte(bic, 0)
+  expect_equal(bic, 12.160578, tolerance = 1e-6)
+  expect_true(is.infinite(calc_BIC(loglik, 2, 0)))
+  expect_warning(calc_BIC(loglik, 2, -10))
+})

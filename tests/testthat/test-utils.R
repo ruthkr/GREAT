90753c6847961f249015f8878a6c0d5d7ad80d84
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-13 21:39:19 +0000

    Re-enabled get_approximate_stretch() and match_names() unit tests

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,32 +1,23 @@
-# brapa_sample_data <- system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR") |>
-#   utils::read.csv() |>
-#   dplyr::select(
-#     gene_id = locus_name,
-#     accession,
-#     timepoint,
-#     replicate = group,
-#     expression_value
-#   )
-#
-#
-# test_that("get_approximate_stretch works", {
-#   approximate_stretch <- get_approximate_stretch(
-#     brapa_sample_data,
-#     reference = "Ro18",
-#     query = "Col0"
-#   )
-#
-#   expect_equal(ceiling(approximate_stretch), 3)
-#   expect_equal(floor(approximate_stretch), 2)
-#   expect_equal(class(approximate_stretch), "numeric")
-# })
-#
-#
-# # test_that("match_names works", {
-# #   a <- LETTERS[1:3]
-# #   b <- LETTERS[4:5]
-# #
-# #   results <- match_names(a, lookup = b, error = NULL, name_string = "names", lookup_vec_last = " and ")
-# #
-# #   expect_error(results, "i.*")
-# # })
+brapa_sample_data <- data.table::fread(system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR"))
+
+test_that("get_approximate_stretch works", {
+  approximate_stretch <- get_approximate_stretch(
+    brapa_sample_data,
+    reference = "Ro18",
+    query = "Col0"
+  )
+
+  # Expected outputs
+  expect_equal(approximate_stretch, 2.6666666, tolerance = 1e-6)
+  expect_gte(approximate_stretch, 0)
+  expect_equal(class(approximate_stretch), "numeric")
+})
+
+test_that("match_names works", {
+  a <- LETTERS[1:3]
+  b <- LETTERS[4:5]
+
+  # Expected outputs
+  expect_error(match_names(x = a, lookup = b))
+  expect_no_error(match_names(x = a, lookup = a))
+})

85bd13618f096b7887ec98a6b09304876c4303e9
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-13 15:34:49 +0000

    updated unit test for utils and calc utils

--- tests/testthat/test-utils-calcs.R
+++ tests/testthat/test-utils-calcs.R
@@ -1,3 +1,22 @@
-test_that("calc_BIC works", {
-  expect_equal(class(calc_BIC(-3, 4, 1)), "numeric")
-})
+# # Get data where have expression data
+# # and fit with a model
+# sample_data <- system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR") |>
+#   utils::read.csv() |>
+#   dplyr::select(
+#     timepoint,
+#     expression_value
+#   ) |>
+#   head(10)
+#
+# fit_model <- lm(timepoint ~ expression_value, data = sample_data)
+#
+# test_that("calc_loglik works", {
+#   expect_equal(class(calc_loglik(fit_model, sample_data)), "numeric")
+# })
+#
+#
+# test_that("calc_BIC works", {
+#   expect_equal(class(calc_BIC(calc_loglik(fit_model, sample_data), 2, 10)), "numeric")
+# })
+#
+#

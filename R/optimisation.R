8b8c057d0da4ac252be97064f9cae5730909557f
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-02-15 10:38:51 +0000

    added calc_overlapping_percent() function and included this function in the optimisation pipeline to make sure that overlapping timepoint - requirement is fulfilled

--- R/optimisation.R
+++ R/optimisation.R
@@ -28,7 +28,7 @@ optimise <- function(data,
 
   # Perform SA using {optimization}
   optimised_params <- optimization::optim_sa(
-    fun = function(x) objective_fun(data, x[1], x[2]),
+    fun = function(x) objective_fun(data, x[1], x[2], overlapping_percent),
     maximization = TRUE,
     start = c(stretch_init, shift_init),
     trace = TRUE,
@@ -50,10 +50,16 @@ optimise <- function(data,
 #' Objective loss function for Simulated Annealing
 #'
 #' @noRd
-objective_fun <- function(data, stretch, shift) {
+objective_fun <- function(data, stretch, shift, overlapping_percent) {
   tryCatch(
     {
+      # Apply registration
       all_data_reg <- apply_registration(data, stretch, shift)
+
+      # Check if overlapping condition is upheld
+      if (calc_overlapping_percent(all_data_reg) < overlapping_percent) stop()
+
+      # Calculate loglik
       loglik_combined <- calc_loglik_H1(all_data_reg)
       return(loglik_combined)
     },
@@ -77,11 +83,11 @@ get_search_space_limits <- function(data, overlapping_percent = 0.5) {
   stretch_lower <- 0.5 * stretch_init
   stretch_upper <- 1.5 * stretch_init
 
-  # Extract timepoint ranges
+  # Extract time point ranges
   timepoints_ref <- unique(data[accession == "ref", timepoint])
   timepoints_query <- unique(data[accession == "query", timepoint])
 
-  # Calculate timepoint ranges
+  # Calculate time point ranges
   range_ref <- diff(range(timepoints_ref))
   range_query <- diff(range(timepoints_query))
   range_query_max_stretch <- stretch_upper * range_query
@@ -111,4 +117,22 @@ get_search_space_limits <- function(data, overlapping_percent = 0.5) {
   )
 
   return(results_list)
+}
+
+#' @noRd
+calc_overlapping_percent <- function(data) {
+  # Extract time point ranges
+  range_ref <- range(unique(data[accession == "ref", timepoint]))
+  range_query <- range(unique(data[accession == "query", timepoint]))
+
+  if (all(range_ref[2] >= range_query[2], range_ref[1] <= range_query[1])) {
+    # Query is fully contained on reference
+    overlapping_percent <- 1
+  } else {
+    # Calculate overlapping percent over reference
+    overlap <- min(c(range_ref[2], range_query[2])) - max(c(range_ref[1], range_query[1]))
+    overlapping_percent <- overlap / diff(range_ref)
+  }
+
+  return(overlapping_percent)
 }

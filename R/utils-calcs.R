cabc77a1de6cd2f3a632a7f4ed8d21eee67d3f40
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-10-18 07:57:52 +0100

    Updated calc_variance() to add separate variance logic for data with replicates and data with no replicates

--- R/utils-calcs.R
+++ R/utils-calcs.R
@@ -54,21 +54,45 @@ calc_variance <- function(all_data, exp_sd = NA) {
   var_poisson <- NULL
   var_range <- NULL
   var <- NULL
+  num_timepoints <- NULL
+  has_replicates <- NULL
 
-  if (any(is.na(exp_sd))) {
-    cli::cli_alert_info("Using estimated standard deviation.")
-    # Calculate Poisson estimate for expression variance
-    all_data[, var_poisson := max(expression_value), by = .(gene_id, accession, timepoint)]
-
-    # Calculate global expression variance
-    all_data[, var_range := (diff(range(expression_value)) / 10)^2, by = .(gene_id, accession)]
-
-    # Select maximum expression variance for each time point
-    all_data[, var := pmax(var_poisson, var_range)]
-    all_data[, c("var_poisson", "var_range") := NULL]
-  } else {
+  if (all(length(exp_sd) == 1, !any(is.na(exp_sd)))) {
     cli::cli_alert_info("Using provided standard deviation {.var exp_sd} = {.val {exp_sd}}.")
     all_data[, var := exp_sd^2]
+  } else {
+    cli::cli_alert_info("Using estimated standard deviation, as no {.var exp_sd} was provided.")
+
+    # Check if data has replicates
+    all_data[, num_timepoints := .N, by = .(gene_id, accession, timepoint)]
+    all_data[, has_replicates := any(num_timepoints > 1), by = .(gene_id, accession)]
+    all_data[, var := numeric()]
+
+    # Split data
+    all_data_reps <- all_data[all_data$has_replicates]
+    all_data_no_reps <- all_data[!all_data$has_replicates]
+
+    # Calculate variance for data with replicates
+    if (nrow(all_data_reps) > 0) {
+      # Calculate Poisson estimate for expression variance
+      all_data_reps[, var_poisson := max(expression_value), by = .(gene_id, accession, timepoint)]
+
+      # Calculate global expression variance
+      all_data_reps[, var_range := (diff(range(expression_value)) / 10)^2, by = .(gene_id, accession, timepoint)]
+
+      # Select maximum expression variance for each time point
+      all_data_reps[, var := pmax(var_poisson, var_range)]
+      all_data_reps[, c("var_poisson", "var_range") := NULL]
+    }
+
+    # Calculate variance for data with no replicates
+    if (nrow(all_data_no_reps) > 0) {
+      all_data_no_reps[, var := diff(range(expression_value)) / 10, by = .(gene_id, accession)]
+    }
+
+    # Combine data
+    all_data <- rbind(all_data_reps, all_data_no_reps)
+    all_data[, c("num_timepoints", "has_replicates") := NULL]
   }
 
   return(all_data)

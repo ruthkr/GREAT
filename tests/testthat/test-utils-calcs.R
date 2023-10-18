53275895b39b90ab3c6942ca935676637aff0123
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2023-10-18 22:52:55 +0100

    Added unit test for calc_variance()

--- tests/testthat/test-utils-calcs.R
+++ tests/testthat/test-utils-calcs.R
@@ -1,6 +1,7 @@
 # Get expression data and fit with a model
 sample_data <- data.table::fread(system.file("extdata/brapa_arabidopsis_all_replicates.csv", package = "greatR"))
-sample_data <- sample_data[1:10, .(timepoint, expression_value)]
+sample_data_raw <- sample_data[1:10]
+sample_data <- calc_variance(data.table::copy(sample_data_raw))
 
 fit_model <- stats::lm(timepoint ~ expression_value, data = sample_data)
 
@@ -23,4 +24,15 @@ test_that("calc_BIC works", {
   expect_equal(bic, 12.160578, tolerance = 1e-6)
   expect_true(is.infinite(calc_BIC(loglik, 2, 0)))
   expect_warning(calc_BIC(loglik, 2, -10))
+})
+
+test_that("calc_variance works", {
+  sample_data_with_reps <- data.table::copy(sample_data_raw)
+  sample_data_with_no_reps <- data.table::copy(sample_data_raw[grep("-a", sample_data_raw$replicate), ])
+  var_with_reps <- calc_variance(sample_data_with_reps)
+  var_with_no_reps <- calc_variance(sample_data_with_no_reps)
+
+  # Expected outputs
+  expect_gt(length(unique(var_with_reps$var)), 1)
+  expect_equal(length(unique(var_with_no_reps$var)), 1)
 })

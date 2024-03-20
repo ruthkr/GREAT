25e8d58287c8967de49069d510b55215dc285864
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2024-03-20 18:45:24 +0000

    Added calculate_distance() unit test to check numerical values of results

--- tests/testthat/test-results.R
+++ tests/testthat/test-results.R
@@ -49,11 +49,21 @@ test_that("plot.res_greatR works", {
 test_that("calculate_distance works", {
   sample_distance <- calculate_distance(registration_results)
 
+  set.seed(3)
+  sample_dist_result <- sample_distance$result[sample(1:nrow(sample_distance$result), 6)]
+  sample_dist_original <- sample_distance$original[sample(1:nrow(sample_distance$original), 6)]
+
   # Expected outputs
   expect_s3_class(sample_distance, "dist_greatR")
   expect_equal(names(sample_distance), c("result", "original"))
   expect_equal(unique(sample_distance$result$timepoint_ref), unique(sample_distance$original$timepoint_ref))
   expect_equal(colnames(sample_distance$result), colnames(sample_distance$original))
+  expect_equal(sample_dist_result$timepoint_ref, c(11, 17, 29, 21, 29, 33))
+  expect_equal(sample_dist_result$timepoint_query, c(20, 14, 10, 37, 2, 37))
+  expect_equal(sample_dist_result$distance, c(0.862, 0.214, 1.460, 4.409, 1.141, 0.801), tolerance = 1e-2)
+  expect_equal(sample_dist_original$timepoint_ref, c(31, 13, 25, 21, 17, 19))
+  expect_equal(sample_dist_original$timepoint_query, c(13, 16, 10, 11, 16, 14))
+  expect_equal(sample_dist_original$distance, c(1.394, 7.272, 0.963, 0.119, 6.526, 0.875), tolerance = 1e-2)
 })
 
 test_that("plot.dist_greatR works", {
